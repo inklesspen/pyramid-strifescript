054dd34815fc1df474ebfc97c90845ae26386921
Jon Rosebaugh <jon@inklesspen.com>
2013-01-30 00:05:55 -0800

    loginKeeper service to manage login requirements

--- strifescript/jstests/unit/servicesSpec.js
+++ strifescript/jstests/unit/servicesSpec.js
@@ -21,15 +21,15 @@ describe('strifescript.services / preloader', function() {
         };
       });
     });
-   inject(function($injector) {
-     $httpBackend = $injector.get('$httpBackend');
-   });
+    inject(function($injector) {
+      $httpBackend = $injector.get('$httpBackend');
+    });
   });
 
- afterEach(function() {
-   $httpBackend.verifyNoOutstandingExpectation();
-   $httpBackend.verifyNoOutstandingRequest();
- });
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
 
   it('should be able to fetch user info', inject(function($rootScope, preloader, loginKeeper) {
     var conflictInfo = ["derp", "herp"];
@@ -41,5 +41,30 @@ describe('strifescript.services / preloader', function() {
     $rootScope.$apply();
     $httpBackend.flush();
     expect(promiseThen).toHaveBeenCalledWith(conflictInfo);
+  }));
+});
+
+describe('strifescript.services / loginKeeper', function() {
+  beforeEach(function() {
+    module('strifescript.services');
+  });
+
+  it('should return a promise which is resolved with the logged in username. should only call the callback once, and keep the provided username', inject(function($rootScope, loginKeeper, $q) {
+    var deferred = $q.defer();
+    var callback = jasmine.createSpy('loginRequiredCallback');
+    callback.andReturn(deferred.promise);
+    loginKeeper.registerLoginRequiredCallback(callback);
+    var promise = loginKeeper.acquireLogin();
+    var promiseThen = jasmine.createSpy('promiseThen');
+    promise.then(promiseThen);
+
+    var promise2 = loginKeeper.acquireLogin();
+    var promiseThen2 = jasmine.createSpy('promiseThen');
+    promise2.then(promiseThen2);
+    deferred.resolve('testUser');
+    $rootScope.$apply();
+    expect(promiseThen).toHaveBeenCalledWith('testUser');
+    expect(promiseThen2).toHaveBeenCalledWith('testUser');
+    expect(callback.calls.length).toBe(1);
   }));
 });
--- strifescript/static/js/services.js
+++ strifescript/static/js/services.js
@@ -6,6 +6,35 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('strifescript.services', []).
+  factory('loginKeeper', ['$q', function($q) {
+    var loginRequiredCallback = angular.noop();
+    var currentUsername = null;
+    var currentPromise = null;
+    return {
+      acquireLogin: function() {
+        if (currentPromise !== null) {
+          return currentPromise;
+        }
+        if (currentUsername === null) {
+          var promise = loginRequiredCallback();
+          promise = promise.then(function(value) {
+            currentUsername = value;
+            currentPromise = null;
+            return value;
+          });
+          currentPromise = promise;
+          return promise;
+        } else {
+          var deferred = $q.defer();
+          deferred.resolve(currentUsername);
+          return deferred.promise;
+        }
+      },
+      registerLoginRequiredCallback: function(callback) {
+        loginRequiredCallback = callback;
+      }
+    };
+  }]).
   factory('preloader', ['$q', '$http', 'loginKeeper', function($q, $http, loginKeeper) {
     return {
       allConflicts: function() {
