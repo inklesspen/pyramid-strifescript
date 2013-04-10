288feaa927880c3618fc13c9be2df8a7245c77ae
Jon Rosebaugh <jon@inklesspen.com>
2013-04-10 20:37:29 +0900

    Remove old services we no longer use.

--- strifescript/static/js/services.js
+++ strifescript/static/js/services.js
@@ -64,48 +64,4 @@ angular.module('strifescript.services', []).
         });
       }
     };
-  }]).
-  factory('oldloginKeeper', ['$q', function($q) {
-    var loginRequiredCallback = angular.noop();
-    var currentUsername = null;
-    var currentPromise = null;
-    return {
-      acquireLogin: function() {
-        if (currentPromise !== null) {
-          return currentPromise;
-        }
-        if (currentUsername === null) {
-          var promise = loginRequiredCallback();
-          promise = promise.then(function(value) {
-            currentUsername = value;
-            currentPromise = null;
-            return value;
-          });
-          currentPromise = promise;
-          return promise;
-        } else {
-          var deferred = $q.defer();
-          deferred.resolve(currentUsername);
-          return deferred.promise;
-        }
-      },
-      registerLoginRequiredCallback: function(callback) {
-        loginRequiredCallback = callback;
-      }
-    };
-  }]).
-  factory('preloader', ['$q', '$http', 'loginKeeper', function($q, $http, loginKeeper) {
-    return {
-      allConflicts: function() {
-        var deferred = $q.defer();
-        var loginPromise = loginKeeper.acquireLogin();
-        loginPromise.then(function(username) {
-          $http.get('/api/conflicts').success(function(data, status, headers, config) {
-            deferred.resolve(data);
-          });
-        });
-        return deferred.promise;
-      }
-    };
-  }]).
-  value('version', '0.1');
+  }]);
